fed86d1e14e9431d03ab2fc8cf271c31b428a95a
LukazAllan <allanlucas130@gmail.com>
2020-03-16 17:13:39 -0300

    Segundo commit

--- main.py
+++ main.py
@@ -4,9 +4,10 @@
 import pygame
 from sys import exit
 from json import load
+from random import choice as rd
 
 # Carregando Dados
-with open('data.json', 'r', encoding='utf-8') as raw:
+with open('data/data.json', 'r', encoding='utf-8') as raw:
     data = load(raw)
 
 # Preparando a Janela
@@ -21,12 +22,12 @@ pygame.display.set_caption('Japonês')
 
 # Cores
 cor = {
-    'b': (0,0,0),# preto
-    'w': (255,255,255), # branco
-    'c': (64,249,255), # ciano
-    'g': (66,255,66), # verde
-    'g': (248,210,16), # 金色
-    'r': (245,23,32), # vermelho
+    'b': (0,0,0),# preto　黒
+    'w': (255,255,255), # branco　白
+    'c': (64,249,255), # ciano　シアン
+    'g': (66,255,66), # verde 緑
+    'g': (248,210,16), # ouro 金色
+    'r': (245,23,32), # vermelho　赤
     'bk': (30,70,130) # only for bk
 }
 
@@ -38,6 +39,11 @@ fonte = pygame.font.SysFont('NSinSum', 80)
 palavra = fonte.render('enpitsu', False, cor['w'])
 pos_palavra = palavra.get_rect()
 pos_palavra.center = (138,40)
+
+letras = []
+#letras = [
+#
+#]
 
 # loop
 while True:
@@ -54,7 +60,8 @@ while True:
     janela.blit(fundo,(0,95))
     pygame.draw.rect(janela, (245,23,32), linha)
     janela.blit(palavra,pos_palavra)
-
+    for c in range(len(letras)):
+        janela.blit(janela[c][])
 
     # Atualizar a tela
     pygame.display.flip()
